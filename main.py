991d2ad1e808eb7a638dce235225ae8ec14a5fbf
wisetree911 <gariksharik224@gmail.com>
2024-08-22 04:17:31 +0500

    Updated example list of items and deleted description from catalog
    updated example list of items and deleted description of items in index.html catalog a

--- main.py
+++ main.py
@@ -9,23 +9,20 @@ def index():
     products = [
         {
             "id": 1,
-            "name": "Товар 1",
-            "description": "Описание товара 1",
-            "price": "1000 руб.",
+            "name": "Nike Jordan",
+            "price": "19000 руб.",
             "image": "product1.jpg"
         },
         {
             "id": 2,
-            "name": "Товар 2",
-            "description": "Описание товара 2",
-            "price": "1200 руб.",
+            "name": "Rick Owens",
+            "price": "120000 руб.",
             "image": "product2.jpg"
         },
         {
             "id": 3,
-            "name": "Товар 3",
-            "description": "Описание товара 3",
-            "price": "1500 руб.",
+            "name": "New Balance",
+            "price": "10000 руб.",
             "image": "product3.jpg"
         }
     ]
@@ -39,25 +36,25 @@ def product_detail(product_id):
     products = [
         {
             "id": 1,
-            "name": "Товар 1",
-            "description": "Описание товара 1",
-            "price": "1000 руб.",
+            "name": "Nike Jordan",
+            "description": "Представляем мужские качественные кроссовки Nike Air Jordan 1 High Satin Black Toe. Силуэт создан крупным брендом и баскетболистом по имени Майкл Джордан.",
+            "price": "19000 руб.",
             "image": "product1.jpg",
             "details": "Подробная информация о товаре 1"
         },
         {
             "id": 2,
-            "name": "Товар 2",
-            "description": "Описание товара 2",
-            "price": "1200 руб.",
+            "name": "Rick Owens",
+            "description": "Высокие розовые кеды с контрастными мысками из гладкой натуральной кожи. Светлая прошитая подошва из термополиуретана с мелким протектором shark tooth, плоская шнуровка с серебристыми люверсами, кожаная подкладка.",
+            "price": "120000 руб.",
             "image": "product2.jpg",
             "details": "Подробная информация о товаре 2"
         },
         {
             "id": 3,
-            "name": "Товар 3",
-            "description": "Описание товара 3",
-            "price": "1500 руб.",
+            "name": "New Balance",
+            "description": "Верх из натуральной замши Текстильные элементы сетчатого плетения Стабилизирующая вставка в пяточной зоне.",
+            "price": "10000 руб.",
             "image": "product3.jpg",
             "details": "Подробная информация о товаре 3"
         }
